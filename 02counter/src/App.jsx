931c5fd1d0e8448209fa62c930d704fc807245f9
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-02-23 18:30:08 +0530

    third commit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
   
-  let counter = 5
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
 
   return (
     <div>
-      <h1>Counter Application</h1>
-      <h2>counter value: {counter}</h2>
-
-      <button>Add Value</button>
+      <h3>Counter App</h3>
+      <p>Counter Value: {counter}</p>
+      <button onClick={addValue}>Add Value</button>
       <br /><br />
-      <button>Remove value</button>
+      <button onClick={removeValue}>Remove Value</button>
     </div>
   )
 }
