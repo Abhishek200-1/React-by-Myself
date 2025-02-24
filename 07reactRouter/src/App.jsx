669d736897f3c2d439d469be42b41b540408d364
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-02-24 19:22:49 +0530

    reactrouter

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div>
-        hello
+        <h2 className='bg-green-600 p-4'>React Router</h2>
     </div>
   )
 }
