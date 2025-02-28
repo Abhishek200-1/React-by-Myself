03bd1e110154d20f5febc2a19fbdac82d7172d6c
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-02-28 17:02:02 +0530

    context api diss

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './App.css'
 import UserContextProvider from './Context/UserContextProvider'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
 
 const App = () => {
+
+
+
   return (
     <UserContextProvider>
       <h1>React with Abhishek Vishwakarma</h1>
+      <Login/>
+      <Profile/>
     </UserContextProvider>
   )
 }
---
+++ 08miniContext/src/Components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useContext } from 'react'
+import UserContext from '../Context/UserContext'
+
+const Profile = () => {
+
+    const  {user} = useContext(UserContext);
+
+    if(!user) return <div>Plase Login!</div>
+    return <div>Welcome {user.username}</div>
+}
+
+export default Profile
